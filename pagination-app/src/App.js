832fa68c8ce0e16be23cf45287b86ab0eb779cbb
Wisit018 <lipton_pink@hotmail.co.th>
2024-03-07 17:34:24 +0700

    Day 1 | Start Project :+1:
    Learn React :100:

---
+++ pagination-app/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import FoodComponent from "./components/FoodComponent";
+import { useState } from "react";
+import MenuData from "./data/MenuData.js";
+function App() {
+  const [foodData, setFoodData] = useState(MenuData);
+  return (
+    <div className="App">
+      <h1>FoodCard | Pagination</h1>
+
+    <div className="container">
+    {foodData.map((data,index) => {
+        return <FoodComponent key={index} {...data}/>
+        
+        
+        })}
+    </div>
+    </div>
+  );
+}
+
+export default App;
